27abd8fe3bcb41b7752ca8e7901f85d3a3bd38ff
CHOKOLADis <53660318+CHOKOLADis@users.noreply.github.com>
2021-04-26 15:26:03 +0400

    7hours in work

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+    var scale=false;
+    $("#selected").on('click',function(){
+        $(".menu").toggle();
+        if (scale==false){
+            $(".arrow").css("transform","scale(1, -1)");
+            scale=true;
+        }
+        else {
+            $(".arrow").css("transform","scale(1, 1)");
+            scale=false;
+        }
+        
+    });
+    var li = $(".menu ul").children("li");
+        li.on('click',function(){
+            $("#selected span").text(" ");
+            $("#selected span").append(li.text);
+        });
+    });
 
 
-transform: scale(1, -1);
+
