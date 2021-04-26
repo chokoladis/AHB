c65d0f2c4fcc6646803cc18d725afe4f7acdd9df
CHOKOLADis <53660318+CHOKOLADis@users.noreply.github.com>
2021-04-26 21:04:10 +0400

    Ending Layout

--- Script/script.js
+++ Script/script.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
     var scale=false;
     $("#selected").on('click',function(){
-        $(".menu").toggle();
-        if (scale==false){
-            $(".arrow").css("transform","scale(1, -1)");
-            scale=true;
-        }
-        else {
-            $(".arrow").css("transform","scale(1, 1)");
-            scale=false;
-        }
         
+        function change_list(){
+            $(".menu").toggle();
+            if (scale==false){
+                $(".arrow").css("transform","scale(1, -1)");
+                scale=true;
+            }
+            else {
+                $(".arrow").css("transform","scale(1, 1)");
+                scale=false;
+            }
+        }
+        change_list();
+
     });
     var li = $(".menu ul").children("li");
         li.on('click',function(){
+            $("li").removeClass("selected_li");
+            $(this).toggleClass("selected_li");
             $("#selected span").text(" ");
-            $("#selected span").append(li.text);
+            var text = $(".selected_li").text();
+            $("#selected span").text(text);
         });
     });
 
